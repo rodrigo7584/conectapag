c914a4a9ccfd8ea18edc41ed6384fcbe5e743344
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-01-24 22:46:36 -0300

    24/01/2024

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btnMobile = document.querySelector('button.btn-mobile-menu')
+function toggleMenu() {
+  const navMobile = document.querySelector('header.menu nav')
+  btnMobile.classList.toggle('active')
+  navMobile.classList.toggle('active')
+}
+btnMobile.addEventListener('click', toggleMenu)
